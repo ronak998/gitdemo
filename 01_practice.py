c318a5ad798603e4554aa000c72bce6bc286f26e
ronak <jainronak163@gmail.com>
2023-12-25 15:59:44 +0530

    changes by asian guy

--- 01_practice.py
+++ 01_practice.py
@@ -2,3 +2,7 @@ print('''Twinkle, twinkle, little star,
 How I wonder what you are!
 Up above the world so high,
 Like a diamond in the sky.''')
+print('''Twinkle, twinkle, little star,
+How I wonder what you are!
+Up above the world so high,
+Like a diamond in the sky.''')
